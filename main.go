d305376fa3d73c94d0f98b13ea8d3e98ed83b6e9
Witek <witek.blach@gmail.com>
2024-05-08 00:58:51 +0200

    mongo connected to ide

--- main.go
+++ main.go
@@ -2,24 +2,50 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
 	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		if request.URL.Path != "/" {
-			http.NotFound(writer, request)
-			return
-		}
-		fmt.Fprintf(writer, "Welcome AAAAAAA to the home page!")
+	err := actualMain()
+
+	if err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
+}
+
+func actualMain() error {
+	godotenv.Load()
+	slog.Info("AAAAAAAAAAAAAAAAA LOADED BITCH")
+	storage, err := NewStorage()
+	if err != nil {
+		return err
+	}
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		result := storage.db.Exec(`
+SELECT * 
+FROM ingredients
+`)
+		fmt.Sprintf("%+v", result)
+		w.Write([]byte("hello world"))
 	})
 
-	mux.HandleFunc("/a", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(writer, "Welcome to the A page!")
+	r.Post("/ingredient", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
 	})
 
-	slog.Info("Listening on port 8080")
-	http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":"+os.Getenv("APP_PORT"), r)
+
+	return nil
 }
